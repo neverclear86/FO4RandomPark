4e08c3056fc089aae80f24beab56fb9995c5d537
neverclear86 <neverclear86sab@gmail.com>
2018-02-19 23:08:31 +0900

    fawerfewagrewa

--- main.js
+++ main.js
@@ -1,45 +1,45 @@
-"use strict";
+"use strict"
 
-const electron = require('electron');
+const electron = require('electron')
 // Module to control application life.
-const app = electron.app;
-const protocol = electron.protocol;
+const app = electron.app
+const protocol = electron.protocol
 // Module to create native browser window.m
-const BrowserWindow = electron.BrowserWindow;
+const BrowserWindow = electron.BrowserWindow
 
-const path = require('path');
-const url = require('url');
+const path = require('path')
+const url = require('url')
 
-const locals = {};
+const locals = {}
 
-let mainWindow;
+let mainWindow
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 800, height: 600})
 
   mainWindow.loadURL(url.format({
     pathname: 'index.html',
     protocol: 'file:',
     slashes: true
-  }));
+  }))
 
   mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+    mainWindow = null
+  })
 
   // 絶対パス修正
   protocol.interceptFileProtocol('file', (req, callback) => {
-    const requestedUrl = req.url.substr(7);
-    console.log(requestedUrl);
+    const requestedUrl = req.url.substr(7)
+    console.log(requestedUrl)
     if (path.isAbsolute(requestedUrl)) {
-      callback(path.normalize(path.join(__dirname, 'docs', requestedUrl)));
+      callback(path.normalize(path.join(__dirname, 'docs', requestedUrl)))
     } else {
-      callback(requestedUrl);
+      callback(requestedUrl)
     }
-  });
-});
+  })
+})
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
